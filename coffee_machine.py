7928f0daab7a69c1d62810b20f06a4b75ab66633
Chrisbal <christianbalinda723@gmail.com>
2023-10-16 20:27:04 +0200

    day 15 challenge

---
+++ coffee_machine.py
@@ -0,0 +1,22 @@
+
+
+# 3 hot flavors
+coffees = [{'name': 'expresso', 'water': 50, 'coffee': 18, 'milk': 0},
+           {'name': 'latte', 'water': 200, 'coffee': 24, 'milk': 150},
+           {'name': 'cappuccino', 'water': 250, 'coffee': 24, 'milk': 100}]
+
+# Resources
+# water: 300ml , coffee: 100g , milk: 200ml
+
+# 2. Coin Operated
+# Four coins: penny: 1 cent, dime: 10 cents, nickel: 5 cents,
+# quarter: 25 cents
+
+# 3. ==> Print report, how much water have left, how much milk, how much
+# coffee?, how much money the coffee machine do have?
+# report
+# ==> Check if the resources sufficient?
+
+# 3.Process coins
+# 4. Check the transactions successful?
+# 5. Make the coffee
